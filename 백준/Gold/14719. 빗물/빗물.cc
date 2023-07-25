dbc507b588408294845daaa293972464faaaa33e
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2023-07-25 13:27:41 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -1,60 +1,62 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
-int H, W;
-vector <int> v;
-int wall[501] = {0,};
-int firstWall;	// 처음으로 벽이 들어온 인덱스 기록
-int cnt = 0;
+/*
+양끝의 높이를 확인
+둘 중 작은 높의 값보다 큰 높이의 벽이 있는 idx 확인
+*/
 
+int w,h;
+int height[500];
 
-void init() {
-	cin >> H >> W;
-	int flag = 0;
-	for (int i = 0; i < W; i++) {
-		int a;
-		cin >> a;
-		v.push_back(a);
-
-		if (a != 0) {	// 일단 벽이 있는 인덱스 기록
-			if (flag == 0) {
-				firstWall = i;
-				flag = 1;
-			}
-		}
-	}
-	memset(wall, -1, sizeof(wall));
-	for (int i = 1; i <= v[firstWall]; i++) wall[i] = firstWall;
+void init(){
+    cin>>h>>w;
+    for(int i = 0; i<w; i++)
+    {
+        cin>>height[i];
+    }
 }
 
+int sum;
+void solution(){
+    for(int i = 1; i<w-1; i++)
+    {   
+        int left_max=height[i];
+        int right_max=height[i];
 
+        for(int a = i-1; a>=0; a--)
+        {
+            if(left_max<height[a])
+            {
+                left_max=height[a];
+            }
+        }
+        for(int b = i+1; b<w; b++)
+        {
+            if(right_max<=height[b])
+            {
+                right_max=height[b];
+            }
+        }
 
-int calc() {
-	for (int i = firstWall; i < W-1; i++) {
-		for (int j = 1; j <= v[i]; j++) {
-			wall[j] = i;
-		}
-		if (v[i] < v[i + 1]) {	// 내 뒤가 나보다 높으면 빗물이 고인다.
-			for (int j = 1; j <= v[i + 1]; j++) {	// 다음 높이까지
-				if (wall[j] == -1) continue;
-				cnt += (i - wall[j]);
-			}
-		}
-	}
-	return cnt;
+        int maxi = min(left_max, right_max);
+        
+        sum+=(maxi - height[i]);
+
+    }
+
+    
 }
 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	init();
-	int ans = calc();
-	cout << ans;
-
-	return 0;
+    init();
+    solution();
+    cout<<sum;
+    return 0;
 }
