6466a6d202c72a94d20344d66aec2168cf5caab4
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2023-08-21 16:53:31 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 224 ms, Memory: 15580 KB -BaekjoonHub

---
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/*
+    (n-1)!
+    균등한 우주 쌍 찾기
+    idx를 같이 저장하고 값에 대해서 정렬
+*/
+int n,m;
+vector<pair<int,int>> planet[10000];
+void init(){
+    cin>>m>>n;
+    for(int i = 0; i<m; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            int size;
+            cin>>size;
+            planet[i].push_back({size, j});
+        }
+        sort(planet[i].begin(), planet[i].end());
+    }
+}
+void solution(){
+    int visited[100][100]={0};
+    int cnt=0;
+    for(int i = 0; i<m; i++)
+    {
+        for(int j = i+1; j<m; j++)
+        {
+            int flag=0;
+            if(visited[i][j]==1)continue;
+
+            for(int a = 0; a<n; a++)
+            {
+                if(planet[i][a].second != planet[j][a].second)
+                {
+                    flag=1;
+                    break;
+                }
+                if(a!=n-1)
+                {
+                    if(planet[i][a+1].first - planet[i][a].first>0)
+                    {
+                        if(planet[j][a+1].first - planet[j][a].first==0)
+                        {
+                            flag=1;
+                            break;
+                        }
+                    }
+                    if(planet[i][a+1].first - planet[i][a].first==0)
+                    {
+                        if(planet[j][a+1].first - planet[j][a].first>0)
+                        {
+                            flag=1;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            if(!flag)
+            {
+                cnt++;
+                visited[i][j]=1;
+                visited[j][i]=1;
+            }
+        }
+    }
+    cout<<cnt;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    init();
+    solution();
+
+    return 0;
+}
