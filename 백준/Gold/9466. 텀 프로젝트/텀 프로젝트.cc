e5b80a72e6f0f77ca6898f39d372466ee53c96e9
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2023-09-02 19:49:47 +0900

    [Gold III] Title: 텀 프로젝트, Time: 524 ms, Memory: 6196 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -3,17 +3,11 @@
 using namespace std;
 
 /*
-    4-7-6-4
+    어느팀에도 속하지 않는 학생들의 수 출력
 
-    1-3
-    2-1-3
-    5-3
-    연결되어있어야함
-
-    어느팀에도 속하지 않는 학생들의 수
-    학생 번호마다 돌아가면서 본인 번호로 돌아오는지 확인
-    이 과정에 포함된 번호들은 다시 돌지 않음
-    dfs로 그룹된 경우는 표시를 해줌
+    학생 번호마다 dfs 돌면서 본인 번호로 돌아오는지 확인
+    dfs로 팀에 속하는지에 대한 여부를 저장 해줌
+    팀에 속하는지에 대한 여부가 결정된 번호들은 다시 돌지 않음
 
 */
 int n, st[100001];
@@ -26,7 +20,6 @@ void init(){
         cin>>st[i];
     }
     memset(check, 0, sizeof(check));
-    memset(visited, 0, sizeof(visited));
 }
 int flag=0;
 void dfs(int id, int now){
@@ -77,13 +70,14 @@ int main(){
         int cnt=0;
         for(int i = 1; i<=n; i++)
         {
-            if(check[i]==1||check[i]==-1)continue;
+            if(check[i]==1)continue;
+            if(check[i]==-1){
+                cnt++; 
+                continue;
+            }
             dfs(i,st[i]);
-        }
-
-        for(int i = 1; i<=n; i++)
             if(check[i]!=1) cnt++;            
-        
+        }
         cout<<cnt<<"\n";
     }
 
