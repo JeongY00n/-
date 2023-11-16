ff4fb7b9f713114c6bc036cfc8a589223650c223
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2023-11-16 14:34:58 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -5,7 +5,7 @@ using namespace std;
 /*
     1인 경우 계산안해도 됨 dp[1]=0;
     주어진 숫자에 대해 2로 나눌 수 있는지 3으로 나눌 수 있는지 확인 필요
-    최소 연산 횟수를 기록
+    연산 횟수를 기록
 */
 
 int main(){
@@ -15,16 +15,16 @@ int main(){
 
     int n; cin>>n;
     int dp[1000001]={0};
-    fill(dp, dp+1000001, 2e9);
+    //fill(dp, dp+1000001, 2e9);
 
     dp[1]=0;
     for(int i = 2; i<=n; i++)
     {
+        dp[i]=dp[i-1]+1;
         if(i%2==0)
             dp[i]=min(dp[i/2]+1, dp[i]);
         if(i%3==0)
             dp[i]=min(dp[i/3]+1, dp[i]);
-        dp[i]=min(dp[i-1]+1, dp[i]);
     }
     cout<<dp[n];
     return 0;
