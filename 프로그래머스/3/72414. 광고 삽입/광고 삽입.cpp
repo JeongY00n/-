f84381b8b681cf51043069bb86fc2eee806bd7f2
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-01-10 22:01:25 +0900

    [level 3] Title: 광고 삽입, Time: 0.36 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
@@ -1,125 +1,71 @@
-#include <iostream>
 #include <string>
 #include <vector>
-
+#include <iostream>
+#include <cstdio>
 using namespace std;
-/*
-    play_time : 동영상이 끝나는 시각
-    adv_time : 광고 시간 길이
-    
-    * 시간을 모두 초로 바꾸고 해당 초에 시청하는 시청자 cnt
-    * 시청자가 있으면 시청자 수 cnt
-    * 만약 cnt를 시작했던 시각부터 adv_time 시간이 지나면,
-      cnt를 시작했던 시각에 +1하고 시청자 수를 빼준다
-    * 그러면서 max cnt에 대한 시간을 저장
-    
-*/
-int ptime[3], atime[3];
-int ptotal, atotal;
-long long check[10000000];
-string answer = "";
-void findanswer()
-{
-    int left=0, right=0;
-    int start_time=0;
-    long long maxi=0, cnt=0;
-    while(right<=ptotal){
-        if(right-left==atotal)
-        {
-            // 종료시각 이전까지 계산한 cnt에 대해 조건 확인 해주기
-            if(maxi < cnt){
-                maxi = cnt;
-                start_time = left;
-            }
-            // 조건 확인이 끝나면 종료시각의 cnt 더해주기
-            cnt-=check[left];
-            cnt+=check[right];
-            left++;
-            right++;
-            
-        }
-        else
-        {
-            cnt+=check[right];
-            right++;
-        }
-    }
 
-    int h = start_time/(60*60);
-    int m = start_time%(60*60)/60;
-    int s = start_time%(60*60)%60;
-
-    if(h<10)
-        answer=answer+'0'+(char)(h+'0')+':';
-    else
-        answer=answer+(char)(h/10+'0')+(char)(h%10+'0')+':';
-    if(m<10)
-        answer=answer+'0'+(char)(m+'0')+':';
-    else
-        answer=answer+(char)(m/10+'0')+(char)(m%10+'0')+':';
-    if(s<10)
-        answer=answer+'0'+(char)(s+'0');
-    else
-        answer=answer+(char)(s/10+'0')+(char)(s%10+'0');
-    
-}
+int arr[360000];
 string solution(string play_time, string adv_time, vector<string> logs) {
-    
-    // 동영상 재생시간
-    int j=0;
-    for(int i = 0; i<play_time.size(); i++)
+
+    for (auto x : logs)
     {
-        if(play_time[i]=='"')
-            continue;
-        else if(play_time[i]==':')
-            j++;
-        else
-            ptime[j]=ptime[j]*10+(play_time[i]-'0');
+        int from = stoi(x.substr(0, 2)) * 3600 + stoi(x.substr(3, 2)) * 60 + stoi(x.substr(6, 2));
+        int to = stoi(x.substr(9, 2)) * 3600 + stoi(x.substr(12, 2)) * 60 + stoi(x.substr(15, 2));
+
+        for (int i = from + 1; i <= to; i++)
+            arr[i]++;
     }
-    ptotal = ptime[0]*60*60+ptime[1]*60+ptime[2];
-    
-    // 광고 재생시간
-    j=0;
-    for(int i = 0; i<adv_time.size(); i++)
+
+    int length = stoi(adv_time.substr(0, 2)) * 3600 + stoi(adv_time.substr(3, 2)) * 60 + stoi(adv_time.substr(6, 2));
+    int playtime = stoi(play_time.substr(0, 2)) * 3600 + stoi(play_time.substr(3, 2)) * 60 + stoi(play_time.substr(6, 2));
+
+    long long int sum = 0;
+    long long int maximum = -1;
+    int index = 1;
+
+    for (int i = 1; i <= length; i++)
+        sum += arr[i];
+    maximum = sum;
+
+    for (int i = 2; i <= (playtime - length + 1); i++)
     {
-        if(adv_time[i]=='"')
-            continue;
-        else if(adv_time[i]==':')
-            j++;
-        else  
-            atime[j]=atime[j]*10+(adv_time[i]-'0');
-    }
-    
-    atotal = atime[0]*60*60+atime[1]*60+atime[2];
-    
-    for(int i = 0; i<logs.size(); i++)
-    {
-        int start=0,end=0;
-        int ltime[2][3]={}, n=0, m=0;
-        for(int j = 0; j<logs[i].size(); j++)
+        sum += (long long int)(arr[i + length - 1] - arr[i - 1]);
+
+        if (sum > maximum)
         {
-            if(logs[i][j]=='"')
-                continue;
-            else if(logs[i][j]==':')
-                m++; 
-            else if(logs[i][j]=='-')
-            {
-                n++;
-                m=0; 
-            }
-            else
-                ltime[n][m]=ltime[n][m]*10+(logs[i][j]-'0');
+            maximum = sum;
+            index = i;
         }
-        
-        start = ltime[0][0]*60*60+ltime[0][1]*60+ltime[0][2];
-        end = ltime[1][0]*60*60+ltime[1][1]*60+ltime[1][2];
-        
-        for(int j = start; j<end; j++)
-            check[j]++;
-        
     }
-    
-    findanswer();
-    
+
+
+
+
+    index--;
+    string answer;
+    int hour, minute, second;
+
+    hour = index / 3600;
+    index -= hour * 3600;
+    minute = index / 60;
+    second = index % 60;
+
+    if (hour <= 9)
+        answer.push_back('0');
+    answer += (to_string(hour));
+    answer.push_back(':');
+
+    if (minute <= 9)
+        answer.push_back('0');
+    answer += (to_string(minute));
+    answer.push_back(':');
+    if (second <= 9)
+        answer.push_back('0');
+    answer += (to_string(second));
+
+
+    if(answer.size() != 8)
+        while(1);
+
     return answer;
 }
