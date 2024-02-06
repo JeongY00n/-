1bfe786c07cb67be679f336a06faa7df597c6adb
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-02-06 14:29:37 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,62 +1,47 @@
 #include<iostream>
-#include<queue>
+#include<algorithm>
 using namespace std;
 
 /*
-    dfs로 현재 높이보다 더 낮은 지점에 대해서만 이동
-    dp사용
-    해당 위치에서 목적지까지 갈 수 있는 경로의 수를 저장
-    상하좌우
-    이동 가능한 모든 경로의 수
+    갈 수 없는 경로를 구분하기 위해서 dp를 -1로 초기화 해줄 필요가 있음
 */
-
-struct Coord{
-    int y,x;
-};
-
-int m,n,map[500][500];
-int answer;
+int M,N;
+int map[500][500];
 int dp[500][500];
 void init(){
-    cin>>m>>n;
-    for(int i = 0; i<m; i++)
-        for(int j = 0; j<n; j++)
+    cin>>M>>N;
+    for(int i = 0; i<M; i++)
+        for(int j = 0; j<N; j++)
             cin>>map[i][j];
-
-    fill(&dp[0][0], &dp[499][499], -1);
-    dp[m-1][n-1]=1;
+    fill(&dp[0][0], &dp[499][500], -1);
+    dp[M-1][N-1]=1;
 }
 
-int fy[] = {-1,1,0,0};
-int fx[] = {0,0,-1,1};
-
-void dfs(int y, int x){
-    
+int fy[]={-1,1,0,0};
+int fx[]={0,0,-1,1};
+void solution(int y, int x){
+    // 지나간적 있는 위치에 대해서는 더이상 진행하지 않음
     if(dp[y][x]!=-1)
-    {
         return;
-    }
 
+    // 지나갔음을 표시하기
     dp[y][x]=0;
 
     for(int i = 0; i<4; i++)
     {
-        int ny = y + fy[i];
-        int nx = x + fx[i];
+        int ny = y+fy[i];
+        int nx = x+fx[i];
 
-        if(ny<0||nx<0||ny>=m||nx>=n)continue;
-        if(map[ny][nx]>=map[y][x])continue;
+        if(ny<0||nx<0||ny>=M||nx>=N)continue;
+        if(map[y][x]<=map[ny][nx])continue;
 
-
-
-        // 이전에 이미 지나간 적이 있는 경우
-        // 저장된 값(경로 수) 더해줌
-        if(dp[ny][nx]>0){
+        if(dp[ny][nx]>0)
+        {
             dp[y][x]+=dp[ny][nx];
             continue;
         }
-        
-        dfs(ny,nx);        
+
+        solution(ny,nx);
         dp[y][x]+=dp[ny][nx];
     }
 }
@@ -67,7 +52,7 @@ int main(){
     cout.tie(0);
 
     init();
-    dfs(0,0);
+    solution(0,0);
     cout<<dp[0][0];
     return 0;
 }
