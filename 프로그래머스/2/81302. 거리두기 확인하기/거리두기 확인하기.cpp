d59a46b56c346c18aa4d2479086b9a4108b12f69
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-02-16 17:36:29 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+/*
+    P : 응시자
+    O : 빈 테이블
+    X : 파티션
+
+*/
+
+int fy[]={-1,1,0,0};
+int fx[]={0,0,-1,1};
+int visited[5][5];
+int flag=0;
+void solution(string* map, int y1, int x1, int y2, int x2)
+{
+    if(flag)return;
+    if(abs(y1-y2)+abs(x1-x2)>2)
+        return;
+    else if(y1!=y2||x1!=x2)
+    {
+        if(map[y2][x2]=='P')
+        {
+            flag=1;
+            return;
+        }
+            
+    }
+    
+    for(int i = 0; i<4; i++)
+    {
+        int ny = y2+fy[i];
+        int nx = x2+fx[i];
+
+        if(ny<0||nx<0||ny>=5||nx>=5)continue;
+        if(visited[ny][nx]==1)continue;
+        if(map[ny][nx]=='X')continue;
+        visited[ny][nx]=1;
+
+        solution(map, y1,x1, ny,nx);
+
+        visited[ny][nx]=0;
+    }
+    
+    
+}
+
+vector<int> solution(vector<vector<string>> places) {
+    vector<int> answer;
+    
+    for(int i = 0; i<5; i++)
+    {
+        string map[5]={};
+        vector<pair<int, int>> yx;
+        for(int j = 0; j<5; j++)
+        {
+            map[j]=places[i][j];
+            for(int p=0; p<5; p++)
+            {
+                if(places[i][j][p]=='P')
+                    yx.push_back({j,p});
+            }
+        }
+        cout<<"\n";
+        if(yx.size()==0)
+            answer.push_back(1);
+        else
+        {
+            // 초기화
+            memset(visited,0,sizeof(visited));
+            flag=0;
+            for(int p=0; p<yx.size(); p++)
+            {
+                // 시작 좌표 초기세팅
+                visited[yx[p].first][yx[p].second]=1;
+                
+                solution(map, yx[p].first, yx[p].second, yx[p].first, yx[p].second);
+                if(flag)break;
+                
+                // memset(visited,0,sizeof(visited));
+                // visited[yx[p].first][yx[p].second]=1;
+            }
+            if(flag)
+                answer.push_back(0);
+            else
+                answer.push_back(1);
+        }
+        
+    }
+    
+    return answer;
+}
