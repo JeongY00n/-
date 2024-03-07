e05af5eb6df69cdb506294dabbc969f7de5d1982
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-03-07 22:33:17 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2156 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -4,13 +4,10 @@
 
 using namespace std;
 
-/*
-    정렬
-*/
 struct Medal{
     int id,g,s,b;
-
 };
+
 bool cmp(Medal a, Medal b){
     if(a.g<b.g) return false;
     else if(a.g==b.g)
@@ -23,17 +20,23 @@ bool cmp(Medal a, Medal b){
     }
     return true;
 }
+
 int N,M;
 vector<Medal> v;
+int g, s, b;
 void init(){
     cin>>N>>M;
     for(int i = 0; i<N; i++)
     {
-        int id,g,s,b;
-        cin>>id>>g>>s>>b;
-        v.push_back({id,g,s,b});
+        int nid,ng,ns,nb;
+        cin>>nid>>ng>>ns>>nb;
+        v.push_back({nid,ng,ns,nb});
+        if(nid==M)
+        {
+            g=ng;s=ns;b=nb;
+        }
     }
-    sort(v.begin(), v.end(), cmp);
+    // sort(v.begin(), v.end(), cmp);
     // for(int i = 0; i<N; i++)
     // {
     //     cout<<v[i].g<<","<<v[i].s<<","<<v[i].b<<"\n";
@@ -41,34 +44,27 @@ void init(){
 }
 void solution(){
 
-    int rank[1001],add=1;
-    rank[v[0].id]=1;
-    if(v[0].id==M)
+    int rank=1;
+
+    for(int i = 0; i<N; i++)
     {
-        cout<<rank[M];
-        return;
-    }
-    for(int i = 1; i<N; i++)
-    {
-        int id=v[i-1].id, g=v[i-1].g, s=v[i-1].s, b=v[i-1].b;
         int nid=v[i].id, ng=v[i].g, ns=v[i].s, nb=v[i].b;
-        if(ng==g&&ns==s&&nb==b)
+
+        if(ng>g)
+            rank++;
+        else if(ng==g)
         {
-            add++;
-            rank[nid]=rank[id];
-        }
-        else
-        {
-            rank[nid]=rank[id]+add;
-            add=1;
-        }
-        if(nid==M)
-        {
-            cout<<rank[nid];
-            return;
+            if(ns>s)
+                rank++;
+            else if(ns==s)
+            {
+                if(nb>b)
+                    rank++;
+            }
         }
     }
 
+    cout<<rank;
 
 }
 int main(){
