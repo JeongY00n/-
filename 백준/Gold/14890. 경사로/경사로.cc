dc757ab242589dbe6d26f3f519e920c32965537f
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-03-23 22:29:26 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -6,11 +6,11 @@ using namespace std;
 
 /*
     경사로 높이 1, 길이 L
-    지도의 모서리 칸 하나하나 확인
+    지도에서 상->하, 좌->우 모서리 확인
+    이동하려는 다음 칸의 높이가 낮은 경우 넘어감
     높이가 같다면 진행방향으로 계속 나아감
-    만약 1크기만큼 차이나는 칸이 주어졌다면 높은 칸 위치에서 L만큼의 뒤를 확인한 후 경사로를 설치할 수 있다면 설치
+    만약 1크기만큼 높은 칸이 주어졌다면 높은 칸 위치에서 L만큼의 뒤를 확인한 후 경사로를 설치할 수 있다면 설치
 
-    이전에 경사로 설치했는지 체크할 필요있음
 */
 int N, L;
 int map1[MAX][MAX], map2[MAX][MAX];
@@ -25,9 +25,7 @@ void init(){
 }
 
 int solution(int map[][MAX]){
-    int path[100]={0};
     int answer=0;
-    // 상->하
     for(int x = 0; x<N; x++)
     {
         int check[100]={0};
@@ -87,79 +85,11 @@ int solution(int map[][MAX]){
             }
         }
         if(!flag)
-        {
-            path[x]=1;
             answer++;
-        }
+        
         
     }
 
-    // 하->상
-    for(int x = 0; x<N; x++)
-    {
-        if(path[x]==1)continue;
-
-        int check[100]={0};
-        int flag=0;
-        for(int y = N-1; y>=1; y--)
-        {
-            // 높이 차이가 1 초과인 경우
-            if(abs(map[y-1][x]-map[y][x])>1)
-            {
-                flag=1;
-                break;
-            }
-
-            // 다음 칸이 1만큼 높은 경우
-            else if(map[y-1][x]-map[y][x]==1)
-            {
-                int back = y-1+L;
-                
-                // 지도를 벗어나는 경우
-                if(back>=N)
-                {
-                    flag=1;
-                    break;
-                }
-                for(int k = y; k<=back; k++)
-                {
-                   // 경사로 연결된 칸의 높이 차가 1 이상인 경우, 이미 경사로가 설치된 경우
-                    if(map[y-1][x]-map[k][x]!=1 || check[k])
-                    {
-                        flag=1;
-                        break;
-                    }
-                    check[k]++;
-                }
-            }
-            // 다음 칸이 1만큼 낮은 경우
-            else if(map[y-1][x]-map[y][x]==-1)
-            {
-                int back = y-L;
-                //cout<<back<<" ";
-                // 지도를 벗어나는 경우
-                if(back<0)
-                {
-                    flag=1;
-                    break;
-                }
-                
-                for(int k = back; k<=y-1; k++)
-                {
-                   // 경사로 연결된 칸의 높이 차가 1 이상인 경우, 이미 경사로가 설치된 경우
-                    if(map[k][x]-map[y][x]!=-1 || check[k])
-                    {
-                        flag=1;
-                        break;
-                    }
-                    check[k]++;
-                }
-            }
-        }
-        
-        if(!flag)
-            answer++;
-    }
     return answer;
 }
 int main(){
