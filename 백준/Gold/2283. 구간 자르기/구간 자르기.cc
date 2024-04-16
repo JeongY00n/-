4e53aec69b23f906d8a04cd149163c25c1c9c7cd
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-04-16 12:36:23 +0900

    [Gold II] Title: 구간 자르기, Time: 576 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.cc
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.cc
@@ -1,36 +1,28 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
 
 using namespace std;
 
 int N;
 long long K;
-long long map[1000001]; // 몇개의 수직선이 그어진 점인지 확인
-//vector<int> idx[1000001];
-int maxi=0;
+long long map[1000001]; // 점의 개수(2,3,4,5 : 4개)가 아니라 길이를 계산 (2,3,4,5 : 3) 
+int maxi=0, mini=1e9;
 void init(){
     cin>>N>>K;
     for(int i = 0; i<N; i++)
     {
         int a,b; cin>>a>>b;
-        for (int j = a+1; j<=b; j++) map[j]++;
+        for (int j = a; j<b; j++) map[j]++;
         
         maxi=max(maxi, b);
+        mini=min(mini, a);
     }
 }
 void solution(){
-    int left=0, right=0;
-    long long sum=0; // 점의 개수(2,3,4,5 : 4개)가 아니라 길이를 계산 (2,3,4,5 : 3) 
+    int left=0, right=mini;
+    long long sum=0; 
     int flag=0;
 
-    // // 구간에 포함되는 점들이 어느 수직선의 점인지 확인
-    // // 처음 등장하는 수직선의 점의 경우 -1을 해줌
-    // for(int i=0; i<idx[mini].size(); i++)
-    // {
-    //     cnt[idx[mini][i]]++;
-    //     if(cnt[idx[mini][i]]==1)sum--;
-    // }
     while(left<=right&&right<=maxi){
 
         //cout<<left<<","<<right<<","<<sum<<"\n";
@@ -38,29 +30,17 @@ void solution(){
         // right++
         if(sum<K)
         {
-            right++;
             // 해당 점에 위치하는 수직선의 개수 더하기
             sum+=map[right];
-            // 수직선의 길이는 수직선 점의 개수 -1 이므로
-            // 수직선의 번호로 구간에 포함된 수직선의 점의 개수를 파악하여
-            // 처음 구간에 들어온 경우 -1을 빼줌
-            // for(int i=0; i<idx[right].size(); i++)
-            // {
-            //     cnt[idx[right][i]]++;
-            //     if(cnt[idx[right][i]]==1)sum--;
-            // }
+            right++;
+
         }
         // 구간에 포함되는 수직선의 길이가 K보다 큰 경우
         // left++
         else if(sum>K)
         {
-            // for(int i=0; i<idx[left].size(); i++)
-            // {
-            //     cnt[idx[left][i]]--;
-            //     if(cnt[idx[left][i]]==0)sum++;
-            // }
-            left++;
             sum-=map[left];
+            left++;
         }
         if(sum==K)
         {
