075c10e842c50a395e6ae1f27d90c237766f4968
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-05-24 21:32:10 +0900

    [Gold V] Title: 트리 색칠하기, Time: 148 ms, Memory: 22224 KB -BaekjoonHub

---
+++ 백준/Gold/24230. 트리 색칠하기/트리 색칠하기.cc
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+int n, color[200001];
+vector<int> node[200001];
+void init(){
+    cin>>n;
+    for(int i = 1; i<=n; i++)
+        cin>>color[i];
+    
+    for(int i = 0; i<n-1; i++)
+    {
+        int a,b; cin>>a>>b;
+        node[a].push_back(b);
+        node[b].push_back(a);
+    }
+}
+
+int answer, end_flag=0;
+int visited[200001];
+void coloring(int now, int c, int ncnt){
+//    cout<<now<<" "<<answer<<"\n";
+    if(ncnt==n)
+    {
+        end_flag=1;
+        return;
+    }
+    if(end_flag)
+        return;
+
+    for(int i = 0; i<node[now].size(); i++)
+    {
+        int next = node[now][i];
+
+        if(visited[next])continue;
+        visited[next]=1;
+
+        if(color[next]!=c)
+            answer++;
+
+        coloring(next, color[next], ncnt+1);
+    }
+}
+
+void solution(){
+    if(color[1]!=0)
+        answer++;
+    visited[1]=1;
+    coloring(1, color[1], 0);
+    cout<<answer;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    init();
+    solution();
+
+    return 0;
+}
