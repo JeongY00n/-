3487132433b4acbef2b9417e77ee98193adcff33
JeongY00n <86840048+JeongY00n@users.noreply.github.com>
2024-09-19 23:03:27 +0900

    [level 3] Title: 부대복귀, Time: 110.49 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
+    vector<int> answer;
+    vector<int> node[100001];
+    for(int i = 0; i<roads.size(); i++)
+    {
+        node[roads[i][0]].push_back(roads[i][1]);
+        node[roads[i][1]].push_back(roads[i][0]);
+    }
+    
+    queue<pair<int,int>> q;
+    int visited[100001]={0};
+    fill(visited,visited+n+1,-1);
+    
+    q.push({destination,0});
+    visited[destination]=0;
+    
+    while(!q.empty()){
+        pair<int,int> now = q.front(); q.pop();
+        int from=now.first, time=now.second;
+
+        for(int i = 0; i<node[from].size(); i++){
+            if(visited[node[from][i]]!=-1&&visited[node[from][i]]<=time+1)continue;
+            visited[node[from][i]]=time+1;
+            q.push({node[from][i],time+1});
+        }
+    }
+    
+    for(int i = 0; i<sources.size(); i++)
+    {
+        answer.push_back(visited[sources[i]]);
+    }
+    
+    return answer;
+}
